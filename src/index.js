8ca0d98e754dc4c5a53d918de881aacedfe14b46
binAryalChhetri <binayakchhetri1234@gmail.com>
2024-04-16 23:35:04 +0545

    Completed building stars review component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Test() {
+  const [movieStar, setMovieStar] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="black" onSetRating={setMovieStar} />
+      <p>This move has {movieStar} stars</p>
+    </div>
+  );
+}
+
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <Test />
+    <StarRating
+      color="red"
+      size={24}
+      messages={["Terrible", "Bad", "Okay", "Good", "Am azing"]}
+      defaultRating={3}
+    />
+    <StarRating />
   </React.StrictMode>
 );
